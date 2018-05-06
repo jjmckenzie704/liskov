fff532a72b2a83d5f27088e3b3681062241f92c9
kgodwin88 <kgodwin1988@aol.com>
2018-05-06 00:52:59 -0400

    Made several updates to the javascript, html, and css files

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -25,79 +25,80 @@ var granimInstance = new Granim({
   });
   //GAMIN ENDS//
   
-  // Get the modal
-var modal = document.getElementById('modal');
+//   // Get the modal
+// var modal = document.getElementById('modal');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
 
   
-  // Initialize Firebase
-  var config = {
-      apiKey: "AIzaSyDK5bKY5Pqm0_gg_mbnOo6UPmIQHbaZNC4",
-      authDomain: "project1-f4f3e.firebaseapp.com",
-      databaseURL: "https://project1-f4f3e.firebaseio.com",
-      projectId: "project1-f4f3e",
-      storageBucket: "project1-f4f3e.appspot.com",
-      messagingSenderId: "303098997727"
-    };
-    firebase.initializeApp(config);
+//   // Initialize Firebase
+//   var config = {
+//       apiKey: "AIzaSyDK5bKY5Pqm0_gg_mbnOo6UPmIQHbaZNC4",
+//       authDomain: "project1-f4f3e.firebaseapp.com",
+//       databaseURL: "https://project1-f4f3e.firebaseio.com",
+//       projectId: "project1-f4f3e",
+//       storageBucket: "project1-f4f3e.appspot.com",
+//       messagingSenderId: "303098997727"
+//     };
+//     firebase.initializeApp(config);
   
-    var usersRef = firebase.database().ref("/users");
+//     var usersRef = firebase.database().ref("/users");
   
-  var mainDB = firebase.database().ref();
+//   var mainDB = firebase.database().ref();
   
-  usersRef.once("value").then(function(snapshot) {
-      console.log(snapshot.val());
-  })
+//   usersRef.once("value").then(function(snapshot) {
+//       console.log(snapshot.val());
+//   })
   
-  firebase.initializeApp(config);
+//   firebase.initializeApp(config);
 
-  var database = firebase.database();
+//   var database = firebase.database();
 
-  $("#signupbutton").on("click", function(event){
-    event.preventDefault();
+//   $("#signupbutton").on("click", function(event){
+//     event.preventDefault();
+//     var firstName = '';
+//     var LastName = '';
+//     var email = '';
+//     var password = '';
 
-    var email = '';
-    var password = '';
+//     var email = $("#signup-email").val().trim();
+//     var password = $("#signup-psw").val().trim();
+//     var firstName = $("#first-name").val().trim();
+//     var lastName = $("#last-name").val().trim();
+//     console.log(email);
+//     console.log(password);
 
-    var email = $("#signup-email").val().trim();
-    var password = $("#signup-psw").val().trim();
-   
-    console.log(email);
-    console.log(password);
-
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-          errorCode = error.code;
-          errorMessage = error.message;
-        });
+//     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//           errorCode = error.code;
+//           errorMessage = error.message;
+//         });
         
-    });    
+//     });    
 
-    $("#loginbutton").on("click", function(event){
-        event.preventDefault();
+//     $("#loginbutton").on("click", function(event){
+//         event.preventDefault();
+//         var email = '';
+//         var password = '';
     
-        var email = '';
-        var password = '';
-    
-        var email = $("#login-email").val().trim();
-        var password = $("#login-psw").val().trim();
+//         var email = $("#login-email").val().trim();
+//         var password = $("#login-psw").val().trim();
         
-        console.log(email);
-        console.log(password);  
+//         console.log(email);
+//         console.log(password);  
 
-        //log in 
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(email)
-        });
+//         //log in 
+//         firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+//             var errorCode = error.code;
+//             var errorMessage = error.message;
+//             console.log(email)
+//         });
     
-    });
+  //  });
 
 
 
--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+    // Hides the error modal when ok button is pressed
+    $("#exit").on("click", function(){
+        $("#errorMessage").modal("hide");
+    });
 
     $(".navbar-toggle").on("click", function(){
         $(this).toggleClass("active")
-    })
-
+    });
+    // Initial FireBase
     var config = {
         apiKey: "AIzaSyDK5bKY5Pqm0_gg_mbnOo6UPmIQHbaZNC4",
         authDomain: "project1-f4f3e.firebaseapp.com",
@@ -13,165 +17,306 @@ $(document).ready(function(){
         messagingSenderId: "303098997727"
     };
     firebase.initializeApp(config);
+    // Creating a variable for the firebase database
+    
     var database = firebase.database();
-   
-    $("#modalSignIn").on("click", function(){
-        var email = $("#email").val().trim();
-        var password = $("#password").val().trim();
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorMessage);
-          });
+    var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    //  When the login button is clicked allows user to login into the app with email and password
+    $("#loginbutton").on("click", function(event){
+        event.preventDefault();
+        var email = '';
+        var password = '';
+        
+        var email = $("#login-email").val().trim();
+     
+        var password = $("#login-psw").val().trim();
+       
+        //log in 
+        
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                $("#header").text("Error");
+                $("#message").text(errorMessage);
+                $("#errorMessage").modal("show");
+
+        });
+        var user = firebase.auth().currentUser;
+        console.log(user);
+        if(user != "null"){
+            login()
+        }
+        $("#modalForm1")[0].reset();
+        $("#login").modal("hide")
     });
-    $("#createAccount").on("click", function(){
-        var email = $("#email").val().trim();
-        var password = $("#password").val().trim();
+    function login(){
+        $("#header").text("Logged In");
+        $("#message").text("You have Successfully Signed In");
+        $("#errorMessage").modal("show");
+        setTimeout(function(){location.replace("sitepage.html")},(1500));
+        };
+    // Allows user to create an account with a email and password to login into the app
+    $("#signupbutton").on("click", function(event){
+        event.preventDefault();
+        var email = '';
+        var password = '';
+    
+        var email = $("#signup-email").val().trim();
+        var password = $("#signup-psw").val().trim();
+       
+ 
         firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorMessage);
-          });
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              $("#header").text("Error");
+              $("#message").text(errorMessage);
+              $("#errorMessage").modal("show");
+            
+            });
+            if(email === "" || expr.text(email)){
+                $("#header").text("Error");
+                $("#message").text("Please enter a valid Email address");
+                $("#errorMessage").modal("show"); 
+            }
+            else if(password === "" || password.length < 6){
+                $("#header").text("Error");
+                $("#message").text("Please enter a valid password ");
+                $("#errorMessage").modal("show");  
+            }
+            else{
+            $("#modalForm2")[0].reset();
+            $("#signup").modal("hide");
+            $("#header").text("Signed Up");
+            $("#message").text("You have Successfully Signed up");
+            $("#errorMessage").modal("show");
+            setTimeout(function(){location.replace("sitepage.html")},(1500));
+           
+            }
+        });
+        // Any time there is an auth state change hides and shows buttons and changes the userId for saving content
         firebase.auth().onAuthStateChanged(function(user){
             if(user){
-                $("#logout").classList.remove("d-none");
+                $("#main").removeClass("hidden");
+                $("#logout").removeClass("hidden");
                 console.log(user);
-                $("#signIn").classList.add("d-none")
+                $("#signUp").addClass("hidden")
+                $("#logIn").addClass("hidden");
+                var userId = firebase.auth().currentUser.uid;
+                // Allows the user to create there on favorite with artist and song
+                $("#addFavorite").on("click", function(event){
+                    event.preventDefault();
+                    var artist = $("#artist").val().trim();
+                    var song = $("#song").val().trim();
+                    var newArtist = {
+                        name: artist,
+                        song: song,             
+                    };
+                    
+                    // prevents user from just adding song or just adding artist
+                    if(artist === "" || song === ""){
+                        $("#header").text("Error");
+                        $("#message").text("You need to enter a Artist and Song");
+                        $("#errorMessage").modal("show");
+                    }
+                    else if(!(/[a-zA-Z]/.test(artist)) || !(/[a-zA-Z]/.test(song))){
+                        console.log("Enter a Valid artist and song")
+                        $("#header").text("Error");
+                        $("#message").text("You need to enter a valid song and or artist");
+                        $("#errorMessage").modal("show");
+                    }
+                    else{
+                        // sets a reference to the userId and stores the artist and song into firebase
+                        database.ref(userId).push(newArtist);
+                        console.log("inside the function " + userId);
+                    }
+                    $("#artistForm")[0].reset();
+                    $("#artist-search").modal("hide");
+                    });
+                    // Takes the stored information from firebase and displays it on the page
+                    database.ref(userId).on("child_added", function(snapshot){
+                        var name = snapshot.val().name;
+                        var song = snapshot.val().song;
+                        var tableBody = $("#favoritesTable > tbody")
+                        var tableRow = $("<tr>").addClass("favBody").attr("artist", name).attr("song", song);
+                        var tableData1 = $("<td>").text(name);
+                        var tableData2 = $("<td>").text(song);
+                        tableRow.append(tableData1).append(tableData2);
+                        tableBody.append(tableRow);
+                    });
             }
             else{
                 console.log("logged out");
-                //$("#logout").classList.add("d-none");
-                $("#signIn").classList.remove("d-none")
+                $("#logout").addClass("hidden");
+                $("#signUp").removeClass("hidden");
+                $("#logIn").removeClass("hidden")
+                $("#favoritesTable > tbody").empty();
+                $("#main").addClass("hidden");
+               
             };
         });
-    });
-    $("#addFavorite").on("click", function(event){
-        event.preventDefault();
-        var artist = $("#artist").val().trim();
-        var song = $("#song").val().trim();
-        var newArtist = {
-            name: artist,
-            song: song,
-        };
-        database.ref().push(newArtist);
-        $("#artistForm")[0].reset()
-    });
+        $("#logout").on("click", function(){
+            $("#header").text("Logged out");
+            $("#message").text("You have Successfully logged out");
+            $("#errorMessage").modal("show");
+            firebase.auth().signOut().then(function() {
+                // Sign-out successful.
+                
 
-    database.ref().on("child_added", function(snapshot){
-        var name = snapshot.val().name;
-        var song = snapshot.val().song;
-        $("#favoriteTable > tbody").append("<tr value = artist + '+' song><td>" + name + "</td><td>" + song + "</td></tr>")
+              }).catch(function(error) {
+                // An error happened.
+              });
+            });
+    $(document).on("click", ".favBody", function(){
+        var artist = $(this)[0].attributes[1].value;
+        var song = $(this)[0].attributes[2].value;
+        addMagic(artist, song);
     })
- 
-
+    // when an artist and a song are entered performs ajax calls to 3 apis
     $("#addMusic").on("click", function(event){
         event.preventDefault();
+        // Setting variables to make ajax call
         var artist = $("#artist").val().trim();
         var song = $("#song").val().trim();
-        var youtubeApi = "https://www.googleapis.com/youtube/v3/search";
-        var apiKey = "AIzaSyDEym1vh-fifGJYrmrz0WsPCyQU6Uni6aQ";
-        var query = artist + "+" + song;
-        console.log(artist)
-        $.ajax({
-            url: youtubeApi,
-            data:{
-                key: apiKey,
-                part: "snippet",
-                type: "video",
-                q: query,
-                order: "viewCount",
-                maxResults: 2
-            },
-            method: "GET"
-        })
-        .then(function(response){
-            var results = response.items;
-            for(var i = 0; i < results.length; i ++){
-                var title =  $("<h2>").text(results[i].snippet.title);
+       addMagic(artist, song);
+        // Resets the form and hides the modal
+        $("#artistForm")[0].reset();
+        $("#artist-search").modal("hide");
+    });
+
+    function addMagic(artist, song){
+        // clears the divs for the videos, lyrics and the artist bio
+        $("#videos").empty();
+        $("#lyrics").empty();
+        $("#bio").empty();
+       var youtubeApi = "https://www.googleapis.com/youtube/v3/search";
+       var apiKey = "AIzaSyDEym1vh-fifGJYrmrz0WsPCyQU6Uni6aQ";
+       var query = artist + " " + song;
+       var querys = query.replace(" ", "+");
+       console.log(artist)
+       console.log(/[a-zA-Z]/.test(artist) || /[a-zA-Z]/.test(song));
+       if(artist === "" || song === ""){
+           $("#header").text("Error");
+           $("#message").text("You need to enter a Artist and Song");
+           $("#errorMessage").modal("show");
+        }
+        else if(!(/[a-zA-Z]/.test(artist))|| !(/[a-zA-Z]/.test(song))){
+           console.log("Enter a Valid artist and song")
+           $("#header").text("Error");
+           $("#message").text("You need to enter a valid song and or artist");
+           $("#errorMessage").modal("show");
+       }
+       else{
+       // making ajax call to the youtube api
+       $.ajax({
+           url: youtubeApi,
+           data:{
+               key: apiKey,
+               part: "snippet",
+               type: "video",
+               q: querys,
+               order: "viewCount",
+               maxResults: 5
+           },
+           method: "GET"
+       })
+       // Takes the information for the youtube api and appends the title of the song and the video to the page
+       .then(function(response){
+           
+           var results = response.items;
+           var count = 0;
+           for(var i = 0; i < results.length; i ++){
+               console.log(results);
+                var title = results[i].snippet.title
+                console.log(title)
+                var titles =  $("<h2>").text(title);
                 var videoId = results[i].id.videoId;
                 var videoDiv = $("<div>");
                 var videoFrame = $("<iframe>");
                 var source = "https://www.youtube.com/embed/" + videoId;
-                videoFrame.attr("src", source);
-                videoDiv.append(title, videoFrame);
-                $("#videos").append(videoDiv);
+                if(title.includes(song) && count === 0){
+                    videoFrame.attr("src", source).attr("frameborder", 0).attr("width", "600px").attr("height", "400px").attr("allowfullscreen", " ").attr("id", "iframe");
+                    videoDiv.append(titles, videoFrame);
+                    $("#videos").append(videoDiv);
+                    count ++;
+                }
+                else{
+                    
+                }
 
-            }
-            
-        });
+           }
+           
+       });
 
-        
-        $.ajax({
-            url : "https://api.musixmatch.com/ws/1.1/track.search",
-                
+       // Making an ajax call to the Musixmatch api to get the the track id 
+       $.ajax({
+           url : "https://api.musixmatch.com/ws/1.1/track.search",
+               
 
-            data:{
-                apikey: "fec873930376f5f5c372618b84d70381",
-                q: query,
-                page: 1,
-                page_size: 1,
-                s_track_rating: "desc",
-                format: "jsonp",
-                callback: "jsonp_callback" 
-            },
-            dataType: "jsonp",
-            jsonpCallback: 'jsonp_callback',
-            contentType: 'application/json',
-            method: "GET"
-        })
-        .then(function(data){
-            var trackId = data.message.body.track_list[0].track.track_id;
-            var title = data.message.body.track_list[0].track.track_name;
-    
-            $.ajax({
-                url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get?",
-                data:{
-                    apikey: "fec873930376f5f5c372618b84d70381",
-                    track_id: trackId,
-                    format: "jsonp",
-                    callback: "jsonp_callback"
-                },
-                dataType: "jsonp",
-                jsonpCallback: 'jsonp_callback',
-                contentType: 'application/json',
-                method: "Get"
-            })
-            .then(function(data){
-                var lyrics = data.message.body.lyrics.lyrics_body;
-                var lyricsDiv = $("<div>");
-                var hLyrics = $("<h2>").text(title);
-                var pLyrics = $("<p>").text(lyrics);
-                lyricsDiv.append(hLyrics);
-                lyricsDiv.append(pLyrics);
-                $("#lyrics").append(lyricsDiv);
-            })
-        });
-      $.ajax({
-          url: "https://ws.audioscrobbler.com/2.0/",
-          data:{
-              method: "artist.getinfo",
-              api_key: "6b6d7e9814cf2cb428f0a568b1fa3659",
-              format: "json",
-              artist: artist,
-              lang: "en",
-            },
-        })
-          .then(function(response){
-              var bio = response.artist.bio.content;
-              var bioDiv = $("<div>");
-              var name = $("<h2>").text(artist);
-              var pBio = $("<p>").text(bio);
-              bioDiv.append(name);
-              bioDiv.append(pBio);
-              $("#bio").append(bioDiv);
-          })
-        $("#artistForm")[0].reset()
-
-    });
-
-
+           data:{
+               apikey: "fec873930376f5f5c372618b84d70381",
+               q: song,
+               page: 1,
+               page_size: 1,
+               s_track_rating: "desc",
+               format: "jsonp",
+               callback: "jsonp_callback" 
+           },
+           dataType: "jsonp",
+           jsonpCallback: 'jsonp_callback',
+           contentType: 'application/json',
+           method: "GET"
+       })
+       .then(function(data){
+           var trackId = data.message.body.track_list[0].track.track_id;
+           var title = data.message.body.track_list[0].track.track_name;
+   // Making another ajax call to the musixmatch api and getting the lyrics from the track id obtained in the first ajax call
+           $.ajax({
+               url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get?",
+               data:{
+                   apikey: "fec873930376f5f5c372618b84d70381",
+                   track_id: trackId,
+                   format: "jsonp",
+                   callback: "jsonp_callback"
+               },
+               dataType: "jsonp",
+               jsonpCallback: 'jsonp_callback',
+               contentType: 'application/json',
+               method: "Get"
+           })
+           // Taking the info from the ajax call and appending it to the page with the title and the lyrics
+           .then(function(data){
+               var lyrics = data.message.body.lyrics.lyrics_body;
+               var lyricsDiv = $("<div>");
+               var hLyrics = $("<h2>").text(title);
+               var pLyrics = $("<p>").text(lyrics);
+               lyricsDiv.append(hLyrics);
+               lyricsDiv.append(pLyrics);
+               $("#lyrics").append(lyricsDiv);
+           })
+       });
+   // Making an ajax call to the last.fm api to obtain the artist bio
+     $.ajax({
+         url: "https://ws.audioscrobbler.com/2.0/",
+         data:{
+             method: "artist.getinfo",
+             api_key: "6b6d7e9814cf2cb428f0a568b1fa3659",
+             format: "json",
+             artist: artist,
+             lang: "en",
+           },
+       })
+       // Taking the info obtained and appending it to the page
+         .then(function(response){
+             var bio = response.artist.bio.content;
+             var bioDiv = $("<div>");
+             var name = $("<h2>").text(artist);
+             var pBio = $("<p>").text(bio);
+             bioDiv.append(name);
+             bioDiv.append(pBio);
+             $("#bio").append(bioDiv);
+         });
+       };
+   };
    
    
     
